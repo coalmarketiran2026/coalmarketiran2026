83525bedc8c12617d1f319cd92b8734c36fd54ee
CoalMarket Iran <anahytakhanbygy@gmail.com>
2026-08-02 09:50:16 +0330

    force update for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// test update
 export default defineConfig({
   plugins: [react()],
   base: '/coalmarketiran2026/', 
